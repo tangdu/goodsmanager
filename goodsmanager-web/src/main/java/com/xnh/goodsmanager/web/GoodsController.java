f19d9f1f7c181717d60b62ec010b18f8136ab4f3
唐杜 <tangdu@qccr.com>
2017-12-26 16:46:01 +0800

    更新文件

--- goodsmanager-web/src/main/java/com/xnh/goodsmanager/web/GoodsController.java
+++ goodsmanager-web/src/main/java/com/xnh/goodsmanager/web/GoodsController.java
@@ -3,19 +3,24 @@ package com.xnh.goodsmanager.web;
 import com.xnh.goodscenter.facade.ro.GoodsQueryRO;
 import com.xnh.goodscenter.facade.ro.GoodsRO;
 import com.xnh.goodsmanager.integration.service.GoodsFacadeClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 商品管理中心
  *
  * @author tangdu
- * @version $: IndexController.java, v 0.1 2017年12月2017/12/23日 下午4:36 tangdu Exp $
+ * @version $: GoodsController.java, v 0.1 2017年12月2017/12/23日 下午4:36 tangdu Exp $
  */
-@Controller
+@RestController
+@RequestMapping("goods")
 public class GoodsController {
+    private static final Logger LOGGER= LoggerFactory.getLogger(GoodsController.class);
 
     @Autowired
     private GoodsFacadeClient goodsFacadeClient;
@@ -25,10 +30,15 @@ public class GoodsController {
      * @param goodsQueryRO 商品查询对象
      * @return 返回商品信息
      */
-    @GetMapping("/index2")
+    @GetMapping("/queryGoodsById")
     @ResponseBody
-    public GoodsRO ddd(GoodsQueryRO goodsQueryRO) {
-        GoodsRO goodsRO = goodsFacadeClient.queryGoodsList(goodsQueryRO);
+    public GoodsRO queryGoodsById(GoodsQueryRO goodsQueryRO) {
+        GoodsRO goodsRO = null;
+        try {
+            goodsRO = goodsFacadeClient.queryGoodsById(goodsQueryRO);
+        } catch (Exception e) {
+            LOGGER.error("queryGoodsById",e);
+        }
         return goodsRO;
     }
 }
